3e6d1bc292689a2a9cf932efbe7d59fe6ba16923
Leandro Henrique <emtudo@gmail.com>
2021-08-28 15:02:54 -0300

    fix: cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', '/graphql', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', '*/graphql', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
