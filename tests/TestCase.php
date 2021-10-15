9df462ee50716452f2a9f28d82180ab57365310c
Leandro Henrique <emtudo@gmail.com>
2021-10-15 18:11:41 -0300

    create schema testing on tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,12 +6,31 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
 use Illuminate\Contracts\Config\Repository as ConfigRepository;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabaseState;
+use Illuminate\Contracts\Console\Kernel;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     use MakesGraphQLRequests;
     use RefreshDatabase;
+
+    public function runDatabaseMigrations()
+    {
+        $this->artisan('migrate:fresh');
+
+        $this->app[Kernel::class]->setArtisan(null);
+
+        $this->beforeApplicationDestroyed(function () {
+            $this->artisan('migrate:rollback');
+
+            RefreshDatabaseState::$migrated = false;
+        });
+        $exists = \DB::select("select exists(SELECT 1 FROM pg_namespace WHERE nspname = 'testing')")[0];
+        if (!$exists->exists) {
+            \DB::SELECT('CREATE SCHEMA testing');
+        }
+    }
 
     /**
      * Return the full URL to the GraphQL endpoint.
